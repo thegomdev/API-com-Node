d48ebcf6970008ece52faf1ad51e760b75bf5c63
MARCELO GOMES JUNIOR <marcelo_gomes-jun1@estudante.sesisenai.org.br>
2024-01-22 14:51:00 -0300

    Entradas Consulta

--- src/controllers/EntradaController.js
+++ src/controllers/EntradaController.js
@@ -9,7 +9,34 @@ const EntradaController = {
         } catch (error) {
             res.status(500).send(error.message);
         }
-    }
+    },
+
+    getAllEntradas: async (req, res) => {
+        try {
+            const entradasSnapshot = await db.collection('entradas').get();
+            const entradas = [];
+            entradasSnapshot.forEach(doc => {
+                entradas.push({ id: doc.id, ...doc.data() });
+            });
+            res.status(200).json(entradas);
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    },
+
+    getEntradaById: async (req, res) => {
+        try {
+            const entradaRef = db.collection('entradas').doc(req.params.id);
+            const doc = await entradaRef.get();
+            if (!doc.exists) {
+                res.status(404).send('Entrada não encontrada');
+            } else {
+                res.status(200).json({ id: doc.id, ...doc.data() });
+            }
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    },
     
 };
 
--- src/routes/entradaEstoqueRoutes.js
+++ src/routes/entradaEstoqueRoutes.js
@@ -5,4 +5,10 @@ const EntradaController = require('../controllers/EntradaController');
 // Rota para criar uma nova entrada
 router.post('/entradas', EntradaController.createEntrada);
 
+// Rota para obter todas as emtradas
+router.get('/entradas', EntradaController.getAllEntradas);
+
+// Rota para obter uma entrada pelo ID
+router.get('/entradas/:id', EntradaController.getEntradaById);
+
 module.exports = router;
